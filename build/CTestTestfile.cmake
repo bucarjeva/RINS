daa4dd3bc2dc5ca577fe4b4daf56f654199cc621
bucarjeva <jb2662@student.uni-lj.si>
2023-03-10 10:44:06 +0100

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/it/Documents/ROS/src
+# Build directory: /home/it/Documents/ROS/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("exercise1")
+subdirs("exercise2")
---
+++ build/exercise1/CMakeFiles/exercise1_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "/home/it/Documents/ROS/devel/lib/python3/dist-packages/exercise1/msg/_Greeting.py"
+  "/home/it/Documents/ROS/devel/lib/python3/dist-packages/exercise1/msg/__init__.py"
+  "/home/it/Documents/ROS/devel/lib/python3/dist-packages/exercise1/srv/_Reverse.py"
+  "/home/it/Documents/ROS/devel/lib/python3/dist-packages/exercise1/srv/__init__.py"
+  "CMakeFiles/exercise1_generate_messages_py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/exercise1_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
